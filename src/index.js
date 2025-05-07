66db3f82598384ede7790f3eea227f31b35a80ce
waris <amwaris2005@gmail.com>
2025-05-07 11:12:24 +0530

    Initial commit: React authorization form with animations

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Contact from './components/Contact';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
